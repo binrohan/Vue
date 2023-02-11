00089b3a06d3b248d25f2f2373b1427c91e6f472
binrohan <binrohan.cs@gmail.com>
2023-02-11 17:41:17 +0600

    Section 7: Vue CLI

---
+++ 7-vue-cli-a-new-vue-project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'raju',
+          name: 'Raju Hasan',
+          phone: '0155489891',
+          email: 'onk@olosh.com',
+        },
+        {
+          id: 'shuvo',
+          name: 'Shuvo Nandi',
+          phone: '01554826656',
+          email: 'snake@olosh.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+</style>
